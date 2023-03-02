14f03913e7987cca129024cf1b44d23add080333
Nabeel Akbar <studiosnabeel@gmail.com>
2023-03-02 20:43:04 +0500

    Add additional methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: "User"
   belongs_to :post
+
+  def count_updater(post_id)
+    post = Post.find_by(id: post_id)
+    post.comments_counter = Comments.where(Post_id:).count
+    post.save
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: "User"
   belongs_to :post
+
+  def count_updater(post_id)
+    post = Post.find_by(id: post_id)
+    post.likes_counter = likes.where(post_id:).count
+  end
 end
