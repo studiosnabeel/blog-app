7929b777b26c663e96b4179a3aaffa75b4395b37
Nabeel Akbar <studiosnabeel@gmail.com>
2023-03-02 20:50:38 +0500

    Rubocop linters fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
   has_many :comments
   has_many :likes
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
 
   def count_updater(author_id)
     user = User.find_by(id: author_id)
-    user.posts_counter = Post.where(author_id:).count 
+    user.posts_counter = Post.where(author_id:).count
     user.save
   end
 
@@ -13,4 +13,3 @@ class Post < ApplicationRecord
     Comment.where(post_id: id).order(created_at: :desc).limit(5)
   end
 end
-
