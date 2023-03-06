41de1d5a7ebc6d6af2e1e4eceaea7a6a1aa252c9
Nabeel Akbar <studiosnabeel@gmail.com>
2023-03-06 22:50:03 +0500

    Add user controllers view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  get '/posts', to: 'posts#index'
-  
-  get '/posts/:id' , to: 'posts#show'
-  
-  get '/users/:username/posts' , to: 'posts#from_user'
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show]
+
 end
