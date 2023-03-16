bdc2ae6e24fd795aacaabd4187a1923e94ba9dac
Nabeel Akbar <studiosnabeel@gmail.com>
2023-03-16 18:42:15 +0500

    Add new user path when signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
   def after_resetting_password_path_for(_resource)
     root_path
   end
+  
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
